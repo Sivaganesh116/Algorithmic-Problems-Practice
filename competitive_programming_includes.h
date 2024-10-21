43512f701cd4f0733c53a3fd6a9151f1d835e38d
Sivaganesh116 <sivaganesh7785@gmail.com>
2024-10-21 09:25:18 +0530

    Initial Commit: Setup directory structure

---
+++ competitive_programming_includes.h
@@ -0,0 +1,30 @@
+#ifndef COMPETITIVE_PROGRAMMING_INCLUDES_H
+#define COMPETITIVE_PROGRAMMING_INCLUDES_H
+
+// Standard libraries
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <numeric> // For accumulate, gcd, lcm
+#include <limits>  // For numeric limits
+#include <functional> // For function objects (like greater<>)
+#include <cassert> // For assert
+#include <iomanip> // For precision formatting
+
+// C libraries
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#endif // COMPETITIVE_PROGRAMMING_INCLUDES_H
